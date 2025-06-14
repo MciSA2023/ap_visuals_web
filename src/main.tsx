daf3a5bb221c20d7101625c21c55a656609d3dd0
Adhar Singh <130926943+MciSA2023@users.noreply.github.com>
2025-06-14 11:44:57 +0200

    feat: add Hero component with navigation and video background; update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Hero from './components/sections/Hero.jsx'
+import Hero from './components/sections/Hero.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
